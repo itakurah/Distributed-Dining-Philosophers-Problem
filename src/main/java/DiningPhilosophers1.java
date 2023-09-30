31c12501389ae45f4494995cfe91462d1e572fac
itakurah <122729995+itakurah@users.noreply.github.com>
2023-09-30 16:03:14 +0200

    added comments and exported client and server logic

--- src/main/java/DiningPhilosophers1.java
+++ src/main/java/DiningPhilosophers1.java
@@ -1,49 +1,26 @@
-import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class DiningPhilosophers1 {
-    public static void main(String[] args) throws IOException {
-        //InetSocketAddress leftNeighborAdress = new InetSocketAddress(args[2], Integer.parseInt(args[3]));
-        //InetSocketAddress rightNeighborAdress = new InetSocketAddress(args[4], Integer.parseInt(args[5]));
-        InetSocketAddress leftNeighborAdress = new InetSocketAddress("localhost", 5003);
+    public static void main(String[] args) {
+//        InetSocketAddress leftNeighborAdress = new InetSocketAddress(args[2], Integer.parseInt(args[3]));
+//        InetSocketAddress rightNeighborAdress = new InetSocketAddress(args[4], Integer.parseInt(args[5]));
+        InetSocketAddress leftNeighborAdress = new InetSocketAddress("localhost", 5005);
         InetSocketAddress rightNeighborAdress = new InetSocketAddress("localhost", 5002);
 
 //        Philosopher philosopher = new Philosopher(Integer.parseInt(args[0]));
         Philosopher philosopher = new Philosopher(1);
+        Server server = new Server(philosopher, 5001);
+        server.start();
         // Connect to left and right neighbors
-
-//        ServerSocket serverSocket = new ServerSocket(Integer.parseInt(args[1])); // Use a fixed port for listening
-        //ServerSocket serverSocket = new ServerSocket(5001); // Use a fixed port for listening
-
-        final int PORT = 5001;
-        final int MAX_THREADS = 2;
-        ExecutorService executorService = Executors.newFixedThreadPool(MAX_THREADS);
-
-        new Thread(() -> {
-            try (ServerSocket serverSocket = new ServerSocket(PORT)) {
-                System.out.println("Server started on port " + PORT);
-                while (true) {
-                    try {
-                        Socket socket = serverSocket.accept();
-                        System.out.println("Client connected: " + socket.getInetAddress().getHostAddress() + ":" + socket.getPort());
-
-                        // Delegate client handling to a thread from the thread pool
-                        executorService.execute(new ClientHandler(socket, philosopher));
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }).start();
-
         philosopher.connectToLeftNeighbor(leftNeighborAdress);
         philosopher.connectToRightNeighbor(rightNeighborAdress);
+
+        try {
+            // Wait for the server to finish
+            server.getServerLatch().await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         new Thread(() -> {
             while (true) {
@@ -53,6 +30,5 @@ public class DiningPhilosophers1 {
                 philosopher.releaseForks();
             }
         }).start();
-
     }
 }
--- src/main/java/DiningPhilosophers3.java
+++ src/main/java/DiningPhilosophers3.java
@@ -1,51 +1,26 @@
-import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class DiningPhilosophers3 {
-    public static void main(String[] args) throws IOException {
-        //InetSocketAddress leftNeighborAdress = new InetSocketAddress(args[2], Integer.parseInt(args[3]));
-        //InetSocketAddress rightNeighborAdress = new InetSocketAddress(args[4], Integer.parseInt(args[5]));
+    public static void main(String[] args) {
+//        InetSocketAddress leftNeighborAdress = new InetSocketAddress(args[2], Integer.parseInt(args[3]));
+//        InetSocketAddress rightNeighborAdress = new InetSocketAddress(args[4], Integer.parseInt(args[5]));
         InetSocketAddress leftNeighborAdress = new InetSocketAddress("localhost", 5002);
-        InetSocketAddress rightNeighborAdress = new InetSocketAddress("localhost", 5001);
+        InetSocketAddress rightNeighborAdress = new InetSocketAddress("localhost", 5004);
 
 //        Philosopher philosopher = new Philosopher(Integer.parseInt(args[0]));
         Philosopher philosopher = new Philosopher(3);
+        Server server = new Server(philosopher, 5003);
+        server.start();
         // Connect to left and right neighbors
-
-
-//        ServerSocket serverSocket = new ServerSocket(Integer.parseInt(args[1])); // Use a fixed port for listening
-        //ServerSocket serverSocket = new ServerSocket(5003); // Use a fixed port for listening
-
-        final int PORT = 5003;
-        final int MAX_THREADS = 2;
-        ExecutorService executorService = Executors.newFixedThreadPool(MAX_THREADS);
-
-        new Thread(() -> {
-            try (ServerSocket serverSocket = new ServerSocket(PORT)) {
-                System.out.println("Server started on port " + PORT);
-                while (true) {
-                    try {
-                        Socket socket = serverSocket.accept();
-                        System.out.println("Client connected: " + socket.getInetAddress().getHostAddress() + ":" + socket.getPort());
-
-                        // Delegate client handling to a thread from the thread pool
-                        executorService.execute(new ClientHandler(socket, philosopher));
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }).start();
-
-
         philosopher.connectToLeftNeighbor(leftNeighborAdress);
         philosopher.connectToRightNeighbor(rightNeighborAdress);
+
+        try {
+            // Wait for the server to finish
+            server.getServerLatch().await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         new Thread(() -> {
             while (true) {
