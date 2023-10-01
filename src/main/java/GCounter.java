8dc29b387155077259efe1584d2d00c4517ef4a9
itakurah <122729995+itakurah@users.noreply.github.com>
2023-10-01 19:06:13 +0200

    introduced synchronized

--- src/main/java/GCounter.java
+++ src/main/java/GCounter.java
@@ -1,6 +1,7 @@
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
+
 /**
  * A G-Counter is a grow-only counter that can only be incremented
  */
@@ -13,36 +14,43 @@ class GCounter implements Serializable {
      * The map of philosopher ids to counts
      */
     private final Map<Integer, Integer> counters;
+
     /**
      * Create a new G-Counter
+     *
      * @param philosopherId The id of the philosopher that the counter belongs to
      */
     public GCounter(int philosopherId) {
         this.philosopherId = philosopherId;
         this.counters = new HashMap<>();
     }
+
     /**
      * Increment the counter
      */
-    public void increment() {
+    public synchronized void increment() {
         counters.put(philosopherId, counters.getOrDefault(philosopherId, 0) + 1);
     }
+
     /**
      * Query the counter
+     *
      * @return The value of the counter
      */
-    public int query() {
+    public synchronized int query() {
         int sum = 0;
         for (int count : counters.values()) {
             sum += count;
         }
         return sum;
     }
+
     /**
      * Merge this counter with another counter
+     *
      * @param otherCounter The other counter
      */
-    public void merge(GCounter otherCounter) {
+    public synchronized void merge(GCounter otherCounter) {
         for (Map.Entry<Integer, Integer> entry : otherCounter.counters.entrySet()) {
             Integer philosopherId = entry.getKey();
             int count = entry.getValue();
