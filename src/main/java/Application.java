33a16d732247a5428c9c398f1c0ffc19c354f8fa
itakurah <122729995+itakurah@users.noreply.github.com>
2023-10-02 20:11:22 +0200

    support for ping feature & exported logic to Game.java

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -17,33 +17,29 @@ public class Application {
         InetSocketAddress rightNeighborAddress = new InetSocketAddress(args[4], Integer.parseInt(args[5]));
         Philosopher philosopher = new Philosopher(Integer.parseInt(args[0]));
         Server server = new Server(philosopher, Integer.parseInt(args[1]));
-        server.start();
+        // Start server listener
+        logger.debug("Starting server listener");
+        server.startListener();
         // Connect to left and right neighbors
+        logger.debug("Connecting to neighbors");
         philosopher.connectToNeighbor(leftNeighborAddress, Direction.LEFT);
         philosopher.connectToNeighbor(rightNeighborAddress, Direction.RIGHT);
-
+        logger.debug("Connected to neighbors");
         try {
             // Wait for the server to finish
             server.getServerLatch().await();
+            // used to keep the container running in docker
+            Thread.sleep(10000);
         } catch (InterruptedException e) {
             logger.error("Error while waiting for server to finish", e);
         }
-
         // Start game thread
-        new Thread(() -> {
-            while (true) {
-                philosopher.think();
-                philosopher.requestForks();
-                philosopher.eat();
-                philosopher.releaseForks();
-            }
-        }).start();
-
-        // Start counter update thread
-        new Thread(() -> {
-            while (true) {
-                philosopher.updateNeighborCounter();
-            }
-        }).start();
+        logger.debug("Starting game thread");
+        Game game = new Game(philosopher);
+        game.start();
+        // Start neighbor counter
+        philosopher.updateNeighborCounter();
+        // Start ping service
+        philosopher.requestPing();
     }
 }
