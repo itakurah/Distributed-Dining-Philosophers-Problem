1be1ece2f290ffb54a29f242d687a0caa11e0736
itakurah <122729995+itakurah@users.noreply.github.com>
2023-10-04 23:25:41 +0200

    exported neighbor connection and server listener process to its respective classes

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -15,16 +15,10 @@ public class Application {
     public static void main(String[] args) {
         InetSocketAddress leftNeighborAddress = new InetSocketAddress(args[2], Integer.parseInt(args[3]));
         InetSocketAddress rightNeighborAddress = new InetSocketAddress(args[4], Integer.parseInt(args[5]));
-        Philosopher philosopher = new Philosopher(Integer.parseInt(args[0]));
+        Philosopher philosopher = new Philosopher(Integer.parseInt(args[0]), leftNeighborAddress, rightNeighborAddress);
         Server server = new Server(philosopher, Integer.parseInt(args[1]));
         // Start server listener
         logger.debug("Starting server listener");
-        server.startListener();
-        // Connect to left and right neighbors
-        logger.debug("Connecting to neighbors");
-        philosopher.connectToNeighbor(leftNeighborAddress, Direction.LEFT);
-        philosopher.connectToNeighbor(rightNeighborAddress, Direction.RIGHT);
-        logger.debug("Connected to neighbors");
         try {
             // Wait for the server to finish
             server.getServerLatch().await();
